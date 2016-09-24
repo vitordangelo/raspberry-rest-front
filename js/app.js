1a89849291606709ba10532df677bc1bd4d5423c
Vitor <metroid.p2p@gmail.com>
2016-09-24 20:02:51 -0300

    v1

---
+++ js/app.js
@@ -0,0 +1,28 @@
+// $(document).ready(function(){
+//     $.getJSON("http://localhost:8000/api/v1/dht11",
+//     function(result){
+//         var ultimoValorLido = result.data.length;
+//         var temperatura = result.data[ultimoValorLido - 1].temperatura;
+//         console.log(result.data[ultimoValorLido - 1].temperatura);
+//         $('.count').text(temperatura);
+//         $(".on").css("height", 2*temperatura + '%');
+//     });
+// });
+
+$(document).ready(function(){
+    var refreshId = setInterval( function()
+        {
+            $.getJSON("http://192.168.1.142:1510/api/v1/dht11",
+            function(result){
+                var ultimoValorLido = result.data.length;
+                var temperatura = result.data[ultimoValorLido - 1].temperatura;
+                var dataultimaLeitura = result.data[ultimoValorLido - 1].created_at;
+
+
+                console.log(result.data[ultimoValorLido - 1].temperatura);
+                $('.count').text(temperatura + 'ºC');
+                $('.data').text(dataultimaLeitura);
+                $(".on").css("height", 2*temperatura + '%');
+            });
+        }, 5000);
+});
---
+++ rasp-py/dht11.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import sys
+import Adafruit_DHT
+import time
+import urllib2
+import json
+
+while 1:
+    humidity, temperature = Adafruit_DHT.read_retry(11, 22)
+    # print (temperature)
+    # print (humidity)
+
+    data = {"temperatura":temperature, "umidade":humidity}
+
+    req = urllib2.Request('http://192.168.1.142:1510/api/v1/dht11')
+    req.add_header('Content-Type', 'application/json')
+
+    response = urllib2.urlopen(req, json.dumps(data))
+
+    time.sleep(30)
